1bc71c0e8bf6c5d78e3deaed6609395f589d82e8
Upwinded.L <281184040@qq.com>
2023-09-11 21:49:31 +0800

    delete 3rd and bin from repo
    use link to download

--- bin/linux/copy-lib.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-#
-mkdir lib
- 
-dependList=$( ldd ./jxqy | awk '{if (match($3,"/")){ print $3}}' )
-
-cp $dependList ./lib
--- bin/linux/install-runtime.sh
+++
@@ -1,3 +0,0 @@
-#to build in linux
-sudo apt-get install libsdl2-2.0-0 libsdl2-image-2.0-0 libsdl2-ttf-2.0-0 libavcodec58 libavutil56 libavdevice58 libavfilter7 libavformat58 libswresample3 libswscale5
-
