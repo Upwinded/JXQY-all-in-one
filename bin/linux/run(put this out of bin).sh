26ae29ff3ec732ef60e9741872b92ec146fc84a3
Upwinded.L <281184040@qq.com>
2025-08-20 08:40:35 +0800

    Update run(put this out of bin).sh

--- bin/linux/run(put this out of bin).sh
+++ bin/linux/run(put this out of bin).sh
@@ -2,10 +2,10 @@
 # goto the directory
 cd bin/linux/
 # set
-chmod 777 jxqy
+chmod 777 JXQY*
 
 # set lib path
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PWD}/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib:/usr/lib64:/lib:/lib64:/usr/local/lib:/usr/local/lib/x86_64-linux-gnu:/lib/x86_64-linux-gnu:/usr/lib/x86_64-linux-gnu:${PWD}/lib
 
 # run jxqy
-exec ./jxqy
+exec ./JXQY*
